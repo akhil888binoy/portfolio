4fb84ca2a880ea0a0bdc9617e1913dcba21fdfa8
Akhil Binoy <92171031+akhil888binoy@users.noreply.github.com>
2023-03-18 21:28:12 +0530

    add body css

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import NavBar from "./components/Navbar"; // import the Navbar component
 function App() {
   return (
     <div className="App">
-      <NavBar /> {/* include the Navbar component */}
-      <h1>Welcome to my app!</h1>
+      <NavBar />
       {/* other content of your app */}
     </div>
   );
