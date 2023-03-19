d6391e03149809f3d551a6cd2553fc30bd4b868c
Akhil Binoy <92171031+akhil888binoy@users.noreply.github.com>
2023-03-19 12:09:33 +0530

    create contact list

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,5 +1,17 @@
 import React from "react";
 
-export default function ContactList() {
-  return <div className="ui celled list">ContactList</div>;
+export default function ContactList(props) {
+  console.log(props);
+  const renderContactList = props.contacts.map((contacts) => {
+    return (
+      <div className="item">
+        <div className="content">
+          <div className="header">{contacts.name}</div>
+          <div>{contacts.email}</div>
+        </div>
+        <i className="trash alternate outline icon"></i>
+      </div>
+    );
+  });
+  return <div className="ui celled list">{renderContactList}</div>;
 }
