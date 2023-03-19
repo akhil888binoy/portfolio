3490b5a3864c15b46db94c5876f18b68803ecf05
Akhil Binoy <92171031+akhil888binoy@users.noreply.github.com>
2023-03-19 11:52:36 +0530

    add dummy contact

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,31 @@ import React from "react";
 import Header from "./Header";
 import AddContact from "./AddContact";
 import ContactList from "./ContactList";
+
 function App() {
+  const contacts = [
+    {
+      id: "1",
+      name: "Akhil",
+      email: "akhil.in",
+    },
+    {
+      id: "2",
+      name: "Raj",
+      email: "raj.in",
+    },
+    {
+      id: "3",
+      name: "kumar",
+      email: "kumar.in",
+    },
+  ];
+
   return (
-    <div>
-      <Header></Header>
-      <AddContact></AddContact>
-      <ContactList></ContactList>
+    <div className="ui container">
+      <Header />
+      <AddContact />
+      <ContactList />
     </div>
   );
 }
